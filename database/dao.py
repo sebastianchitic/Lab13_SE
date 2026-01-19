ecfa191f7b64c4f934b0c6c4e2915f415590aa36
Sebastian Chitic <126360779+sebastianchitic@users.noreply.github.com>
2026-01-19 14:38:02 +0100

    Update dao.py

--- database/dao.py
+++ database/dao.py
@@ -58,3 +58,31 @@ class DAO:
         cursor.close()
         conn.close()
         return result
+
+@staticmethod
+    def get_archi_pesati():
+        conn = DBConnect.get_connection()
+        result = []
+        cursor = conn.cursor(dictionary=True)
+        
+        # Query modificata usando WHERE per i join
+        query = """
+            SELECT g1.cromosoma as c1, g2.cromosoma as c2, SUM(i.correlazione) as peso
+            FROM interazione i, gene g1, gene g2
+            WHERE i.id_gene1 = g1.id
+              AND i.id_gene2 = g2.id
+              AND g1.cromosoma <> g2.cromosoma
+              AND g1.cromosoma > 0
+              AND g2.cromosoma > 0
+            GROUP BY g1.cromosoma, g2.cromosoma
+        """
+        
+        cursor.execute(query)
+        
+        for row in cursor:
+            # Restituisce una tupla (Cromosoma 1, Cromosoma 2, Peso Totale)
+            result.append((row["c1"], row["c2"], row["peso"]))
+            
+        cursor.close()
+        conn.close()
+        return result
